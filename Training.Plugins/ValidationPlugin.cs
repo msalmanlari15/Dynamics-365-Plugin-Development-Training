9afc74365b427653601fdd3b83785c93949a6d22
Mohammad Salman Lari <salman.lari@acs.com.sa>
2019-11-27 12:56:08 +0300

    Added class ValidationPlugin and wrote code for validation

---
+++ Training.Plugins/ValidationPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+namespace Training.Plugins
+{
+    public class ValidationPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)
+            serviceProvider.GetService(typeof(IPluginExecutionContext));
+            Entity entity = (Entity)context.InputParameters["Target"];
+            string name = (string)entity["ita_name"];
+            if ("Test".Equals(name))
+            {
+                throw new InvalidPluginExecutionException("Cannot use this name");
+            }
+        }
+    }
+}
